4de0f8f01fdeb3eab6d6f3370cc7a83b0fe43a06
connor <mcwvproductions@gmail.com>
2019-04-29 11:28:52 -0700

    add flow-green

---
+++ flow-green.py
@@ -0,0 +1,24 @@
+import requests
+import time
+
+URL = "https://172.28.219.225/api/NA4t9hTylAS7RWzmw4VYXrbSb2VnPpDIA4ctvrWe"
+wait = .2
+
+while 1:
+    # print(requests.get("https://172.28.219.225/api/NA4t9hTylAS7RWzmw4VYXrbSb2VnPpDIA4ctvrWe/lights", verify=False))
+    requests.put(URL + "/lights/1/state", "{\"on\": true}", verify=False)
+    time.sleep(wait)
+    requests.put(URL + "/lights/2/state", "{\"on\": true}", verify=False)
+    time.sleep(wait)
+    requests.put(URL + "/lights/3/state", "{\"on\": true}", verify=False)
+    time.sleep(wait)
+    requests.put(URL + "/lights/4/state", "{\"on\": true}", verify=False)
+    time.sleep(wait)
+    requests.put(URL + "/lights/1/state", "{\"on\": false}", verify=False)
+    time.sleep(wait)
+    requests.put(URL + "/lights/2/state", "{\"on\": false}", verify=False)
+    time.sleep(wait)
+    requests.put(URL + "/lights/3/state", "{\"on\": false}", verify=False)
+    time.sleep(wait)
+    requests.put(URL + "/lights/4/state", "{\"on\": false}", verify=False)
+    # time.sleep(wait)
