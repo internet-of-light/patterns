ddeca4803b6d5f81abcfe975b52b7d8e9642a9b1
connor <mcwvproductions@gmail.com>
2019-05-02 20:01:43 -0700

    add blink-main; for testing lobby bulbs

---
+++ blink-main.py
@@ -0,0 +1,25 @@
+import requests
+import time
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+URL = "https:///api/"
+wait = 1
+
+
+def blink(light_id):
+    print("blinking " + str(light_id))
+    requests.put(URL + "/lights/" + str(light_id) + "/state", "\"on\": true}", verify=False)
+    time.sleep(wait)
+    requests.put(URL + "/lights/" + str(light_id) + "/state", "\"on\": false}", verify=False)
+    time.sleep(wait)
+    requests.put(URL + "/lights/" + str(light_id) + "/state", "\"on\": true}", verify=False)
+    time.sleep(wait)
+    requests.put(URL + "/lights/" + str(light_id) + "/state", "\"on\": false}", verify=False)
+    time.sleep(wait)
+    requests.put(URL + "/lights/" + str(light_id) + "/state", "\"on\": true}", verify=False)
+
+
+for i in range(21):
+    blink(i + 1)
