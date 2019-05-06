1a616d1dc85abed48fd5df423d815e369f95f44b
connor <mcwvproductions@gmail.com>
2019-05-06 10:28:17 -0700

    compressed code

--- encounter-green.py
+++ encounter-green.py
@@ -4,28 +4,15 @@ import time
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-import vlc
+# Green Bridge
+IP = "172.28.219.225"
+uname = "NA4t9hTylAS7RWzmw4VYXrbSb2VnPpDIA4ctvrWe"
 
-# p = vlc.MediaPlayer("file:///test.mp3")
-
-URL = "https://172.28.219.225/api/NA4t9hTylAS7RWzmw4VYXrbSb2VnPpDIA4ctvrWe"
+URL = "https://" + IP + "/api/" + uname
 wait = .2
 
-requests.put(URL + "/groups/1/action", "{\"on\": true, \"xy\":[0.675,0.322]}", verify=False)
-# requests.put(URL + "/groups/1/action", "{\"xy\":[0.675,0.322]}", verify=False)
-# requests.put(URL + "/lights/1/state", "{\"on\": true}", verify=False)
-# requests.put(URL + "/lights/1/state", "{\"xy\":[0.675,0.322]}", verify=False)
-# requests.put(URL + "/lights/2/state", "{\"on\": true}", verify=False)
-# requests.put(URL + "/lights/2/state", "{\"xy\":[0.675,0.322]}", verify=False)
-# requests.put(URL + "/lights/3/state", "{\"on\": true}", verify=False)
-# requests.put(URL + "/lights/3/state", "{\"xy\":[0.675,0.322]}", verify=False)
-# requests.put(URL + "/lights/4/state", "{\"on\": true}", verify=False)
-# requests.put(URL + "/lights/4/state", "{\"xy\":[0.675,0.322]}", verify=False)
+requests.put(URL + "/groups/1/action", "{\"on\": true, \"xy\":[0.675,0.322], \"transitiontime\": 1}", verify=False)
 
-time.sleep(7)
+time.sleep(4)
 
-requests.put(URL + "/groups/1/action", "{\"xy\":[0.300,0.300]}", verify=False)
-# requests.put(URL + "/lights/1/state", "{\"xy\":[0.300,0.300]}", verify=False)
-# requests.put(URL + "/lights/2/state", "{\"xy\":[0.300,0.300]}", verify=False)
-# requests.put(URL + "/lights/3/state", "{\"xy\":[0.300,0.300]}", verify=False)
-# requests.put(URL + "/lights/4/state", "{\"xy\":[0.300,0.300]}", verify=False)
+requests.put(URL + "/groups/1/action", "{\"xy\":[0.300,0.300], \"transitiontime\": 50}", verify=False)
