563144cd5a44e192db3dce4ef39efaf00f8494a0
connor <mcwvproductions@gmail.com>
2019-05-20 01:34:00 -0700

    add pi versions for testing tomorrow

--- pi-win-lowerlobby.py
+++ pi-win-lowerlobby.py
@@ -6,52 +6,55 @@ import time
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-URL = "https://172.28.219.179/api/NA4t9hTylAS7RWzmw4VYXrbSb2VnPpDIA4ctvrWe"
-sleepTime = .2
+URL = "https://172.28.219.179/api/rARKEpLebwXuW01cNVvQbnDEkd2bd56Nj-hpTETB"
+sleepTime = .5
 
-white = "[0.400,0.400]"
+white = "[0.486,0.460]"
 red = "[0.675,0.322]"
 
 #requests.put(URL + "/groups/1/action", "{\"on\": true, \"transitiontime\": 1}", verify=False)
-print("sequence start")
+
+print('sequence start')
+
 time.sleep(sleepTime)
 
-requests.put(URL + "/lights/11/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 1}", verify=False)
+requests.put(URL + "/lights/11/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 5}", verify=False)
 time.sleep(sleepTime)
-requests.put(URL + "/lights/14/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 1}", verify=False)
+requests.put(URL + "/lights/14/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 5}", verify=False)
 time.sleep(sleepTime)
-requests.put(URL + "/lights/16/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 1}", verify=False)
+requests.put(URL + "/lights/16/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 5}", verify=False)
 time.sleep(sleepTime)
-requests.put(URL + "/lights/23/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 1}", verify=False)
+requests.put(URL + "/lights/23/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 5}", verify=False)
 time.sleep(sleepTime)
-requests.put(URL + "/lights/7/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 1}", verify=False)
+requests.put(URL + "/lights/7/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 5}", verify=False)
 time.sleep(sleepTime)
-requests.put(URL + "/lights/21/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 1}", verify=False)
+requests.put(URL + "/lights/21/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 5}", verify=False)
 time.sleep(sleepTime)
-requests.put(URL + "/lights/10/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 1}", verify=False)
+requests.put(URL + "/lights/10/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 5}", verify=False)
 time.sleep(sleepTime)
-requests.put(URL + "/lights/15/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 1}", verify=False)
+requests.put(URL + "/lights/15/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 5}", verify=False)
 time.sleep(sleepTime)
-requests.put(URL + "/lights/22/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 1}", verify=False)
+requests.put(URL + "/lights/22/state", "{\"on\": true, \"xy\":" + red + ", \"transitiontime\": 5}", verify=False)
 time.sleep(sleepTime)
 
 # flash here
 
-print("flash")
+print('flash')
 
-requests.put(URL + "/groups/1/action", "{\"on\": false, \"transitiontime\": 1}", verify=False)
-time.sleep(sleepTime)
-requests.put(URL + "/groups/1/action", "{\"on\": true, \"transitiontime\": 1}", verify=False)
-time.sleep(sleepTime)
-requests.put(URL + "/groups/1/action", "{\"on\": false, \"transitiontime\": 1}", verify=False)
-time.sleep(sleepTime)
-requests.put(URL + "/groups/1/action", "{\"on\": true, \"transitiontime\": 1}", verify=False)
+requests.put(URL + "/groups/1/action", "{\"bri\": 100, \"transitiontime\": 20}", verify=False)
+time.sleep(sleepTime * 5)
+requests.put(URL + "/groups/1/action", "{\"bri\": 254, \"transitiontime\": 20}", verify=False)
+time.sleep(sleepTime * 5)
+requests.put(URL + "/groups/1/action", "{\"bri\": 100, \"transitiontime\": 20}", verify=False)
+time.sleep(sleepTime * 5)
+requests.put(URL + "/groups/1/action", "{\"bri\": 254, \"transitiontime\": 20}", verify=False)
 
 # Transition back
+# This is where a handoff back to the main system would go, too
 
-print("return")
+print('return')
 
-time.sleep(4)
-requests.put(URL + "/groups/1/action", "{\"xy\": " + white + ", \"transitiontime\": 40}", verify=False)
+time.sleep(5)
+requests.put(URL + "/groups/1/action", "{\"on\": true, \"xy\": " + white + ", \"transitiontime\": 40, \"bri\": 254}", verify=False)
 
-print("sequence finish")
+print('sequence finish')
